b37fb1e10c42288969bf3ddd319bec99bef7cb4c
Tuan Nguyen <tuan.nguyen@jmango360.com>
2021-04-12 18:19:51 +0700

    Add redux thunk

--- src/App.js
+++ src/App.js
@@ -2,31 +2,37 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import MomentUtils from '@date-io/moment';
+import { Provider as StoreProvider } from 'react-redux';
+
 import { ThemeProvider } from '@material-ui/styles';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { renderRoutes } from 'react-router-config';
 
+import { configureStore } from './store';
 import theme from './theme';
 import routes from './routes';
 import { ScrollReset, CookiesNotification } from './components';
 import './mixins/moment';
 import './mixins/validate';
-import './mock';
+// import './mock';
 import './assets/scss/index.scss';
 
 const history = createBrowserHistory();
+const store = configureStore();
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <MuiPickersUtilsProvider utils={MomentUtils}>
-        <Router history={history}>
-          <ScrollReset />
-          <CookiesNotification />
-          {renderRoutes(routes)}
-        </Router>
-      </MuiPickersUtilsProvider>
-    </ThemeProvider>
+    <StoreProvider store={store}>
+      <ThemeProvider theme={theme}>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <Router history={history}>
+            <ScrollReset />
+            <CookiesNotification />
+            {renderRoutes(routes)}
+          </Router>
+        </MuiPickersUtilsProvider>
+      </ThemeProvider>
+    </StoreProvider>
   );
 };
 
