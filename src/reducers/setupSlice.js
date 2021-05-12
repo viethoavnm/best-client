7ead39231dc1412985faa647c2cfc21241efc18f
hao <hao@tesosoft.com>
2021-05-12 15:11:14 +0700

    footer

--- src/reducers/setupSlice.js
+++ src/reducers/setupSlice.js
@@ -1,8 +1,8 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { getSetupByKey, getHomeData } from '../services/setup';
-import { MENU_WEB_CONFIG, HOME_WEB_CONFIG } from '../utils/constant';
-import Lodash from 'lodash';
-import { getSafeValue } from 'utils';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { isEmpty } from 'lodash';
+import { convertTranslations, getSafeValue } from 'utils';
+import { getHomeData, getSetupByKey } from '../services/setup';
+import { MENU_WEB_CONFIG, TYPE_MENU, TYPE_MENU_LINK } from '../utils/constant';
 
 export const fetchMenuWeb = createAsyncThunk('setup/fetchMenuWeb', async () => {
   const response = await getSetupByKey(MENU_WEB_CONFIG);
@@ -23,7 +23,8 @@ export const setupSlice = createSlice({
   name: 'setup',
   initialState: {
     menu: [],
-    homeData: []
+    homeData: [],
+    menuData: []
   },
   reducers: {
     // updateMenu: (state, action) => {
@@ -36,6 +37,21 @@ export const setupSlice = createSlice({
   extraReducers: {
     [fetchMenuWeb.fulfilled]: (state, action) => {
       state.menu = action.payload;
+      if (Array.isArray(action.payload)) {
+        let menu = action.payload?.filter(element => {
+          if (
+            !TYPE_MENU_LINK[element?.type] ||
+            (element?.type === TYPE_MENU.CATEGORY &&
+              isEmpty(element?.id_category)) ||
+            (element?.type === TYPE_MENU.POST && isEmpty(element?.id_post))
+          ) {
+            return false;
+          } else {
+            return true;
+          }
+        });
+        state.menuData = menu.map(menu => convertTranslations({ ...menu }));
+      }
     },
     [fetchHomeData.fulfilled]: (state, action) => {
       state.homeData = action.payload;
