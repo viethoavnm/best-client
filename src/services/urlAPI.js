5cfba93e0b4e3db18b396bbb66388d4fbba4b14b
hao <hao@tesosoft.com>
2021-05-13 18:16:01 +0700

    set default api dev

--- src/services/urlAPI.js
+++ src/services/urlAPI.js
@@ -1,4 +1,5 @@
-export const urlBase = process.env.REACT_APP_API_BASE_URL
+export const urlBase =
+  process.env.REACT_APP_API_BASE_URL | 'http://210.245.110.19:5000/v1/';
 
 export const urlGetMenuConfig = 'setup/%s';
 export const urlGetHomeData = 'setup/home-data';
