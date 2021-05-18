677e02013e5e4bb17fefe5643a06968fcbce05ae
bao.tesosoft <bao.tesosoft@gmail.com>
2021-05-18 15:33:20 +0700

    Add banner download app

--- src/views/Home/components/DownloadAppSection/DownloadAppSection.js
+++ src/views/Home/components/DownloadAppSection/DownloadAppSection.js
@@ -1,22 +1,86 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-
+import { Container, Title } from 'components';
 import useStyles from './styles';
+import { Grid, Hidden } from '@material-ui/core';
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
-const DownloadAppSection = props => {
-  const { className, ...rest } = props;
-
+const DownloadAppSection = () => {
   const classes = useStyles();
 
   return (
-    <section>
-      <Card className={clsx(classes.rootCard)} elevation={0}>
-        <img src="images/dowload-app.svg" className={clsx(classes.media)} />
-      </Card>
-    </section>
+    <Fragment>
+      <Hidden smUp>
+        <section>
+          <Container>
+            <Title size="large" className={classes.titleBox}>
+              <h2 className={classes.title}>TẢI APP CỦA CHÚNG TÔI</h2>
+              <Grid container spacing={3}>
+                <Grid item md={6} xs={6}>
+                  <a
+                    className={clsx(classes.downloadButton, classes.appstore)}
+                    onClick={() => alert('Download')}></a>
+                </Grid>
+                <Grid item md={6} xs={6}>
+                  <a
+                    className={clsx(classes.downloadButton, classes.googleplay)}
+                    onClick={() => alert('Download')}></a>
+                </Grid>
+              </Grid>
+            </Title>
+          </Container>
+        </section>
+      </Hidden>
+      <section className={classes.rootCard}>
+        <Container>
+          <Grid container spacing={3}>
+            <Grid item md={7} xs={12}></Grid>
+            <Grid item md={5} xs={12} style={{ textAlign: 'center' }}>
+              <div className={classes.downloadContainer}>
+                <div className={classes.hideOnMobile}>
+                  <h2 className={classes.title}>TẢI APP CỦA CHÚNG TÔI</h2>
+                  <p className={classes.description}>
+                    Tải ứng dụng để có thêm thông tin về nguồn cung cấp sinh
+                    khối và dịch vụ bếp khí hóa sinh khối
+                  </p>
+                  <Grid container spacing={3}>
+                    <Grid item md={6} sm={6} xs={12}>
+                      <a
+                        className={clsx(
+                          classes.downloadButton,
+                          classes.appstore
+                        )}
+                        onClick={() => alert('Download')}></a>
+                    </Grid>
+                    <Grid item md={6} sm={6} xs={12}>
+                      <a
+                        className={clsx(
+                          classes.downloadButton,
+                          classes.googleplay
+                        )}
+                        onClick={() => alert('Download')}></a>
+                    </Grid>
+                  </Grid>
+                  <h2 className={classes.title}>HƯỚNG DẪN SỬ DỤNG APP</h2>
+                  <Link className={classes.instructionButton}>XEM NGAY</Link>
+                </div>
+                <Hidden smUp>
+                  <Grid
+                    container
+                    spacing={3}
+                    style={{ textAlign: 'right', display: 'inline-block' }}>
+                    <h2 className={classes.title}>HƯỚNG DẪN SỬ DỤNG APP</h2>
+                    <Link className={classes.instructionButton}>XEM NGAY</Link>
+                  </Grid>
+                </Hidden>
+              </div>
+            </Grid>
+          </Grid>
+        </Container>
+      </section>
+    </Fragment>
   );
 };
 
--- src/views/Home/components/DownloadAppSection/styles.js
+++ src/views/Home/components/DownloadAppSection/styles.js
@@ -1,14 +1,95 @@
 import { makeStyles } from '@material-ui/styles';
-
+import AppstoreBadge from 'assets/img/appstore-badge.svg';
+import GooglePlayBadge from 'assets/img/googleplay-badge.svg';
+import DownloadAppBg from 'assets/img/download-app-bg.svg';
 const useStyles = makeStyles(theme => ({
   rootCard: {
     width: '100%',
     height: '100%',
-    marginTop: theme.spacing(5)
+    paddingTop: '10%',
+    paddingBottom: '10%',
+    marginTop: theme.spacing(5),
+    backgroundImage: `url(${DownloadAppBg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    [theme.breakpoints.down('xs')]: {
+      backgroundPosition: '70%'
+    }
   },
   media: {
     width: '100%',
     height: '100%'
+  },
+  downloadContainer: {
+    width: '90%',
+    display: 'inline-block'
+  },
+  titleBox: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4)
+  },
+  title: {
+    fontSize: 24,
+    color: '#92BF1F',
+    marginBottom: 16,
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 16
+    }
+  },
+  description: {
+    fontSize: 16
+  },
+  hideOnMobile: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    },
+    display: 'none'
+  },
+  downloadButton: {
+    width: '100%',
+    display: 'block',
+    paddingBottom: '29%',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    [theme.breakpoints.down('sm')]: {
+      paddingBottom: '29%'
+    },
+    [theme.breakpoints.between('sm', 'md')]: {
+      paddingBottom: '17%'
+    }
+  },
+  instructionButton: {
+    lineHeight: '1.5',
+    fontWeight: 600,
+    fontSize: 16,
+    display: 'inline-block',
+    padding: '18.57px 42px 18.57px 42px',
+    background: '#FFFFFF',
+    border: '1px solid #92BF1F',
+    boxSizing: 'border-box',
+    boxShadow: '0px 32px 64px -24px rgba(44, 44, 44, 0.16)',
+    textDecoration: 'none',
+    color: '#426B2F',
+    borderRadius: 8,
+    [theme.breakpoints.down('sm')]: {
+      padding: '13px 42px 13px 42px',
+      color: '#92BF1F'
+    }
+  },
+  googleplay: {
+    [theme.breakpoints.up('sm')]: {
+      backgroundPosition: 'left'
+    },
+    backgroundPosition: 'center',
+    backgroundImage: `url(${GooglePlayBadge})`
+  },
+  appstore: {
+    [theme.breakpoints.up('sm')]: {
+      backgroundPosition: 'right'
+    },
+    backgroundPosition: 'center',
+    backgroundImage: `url(${AppstoreBadge})`
   }
 }));
 
