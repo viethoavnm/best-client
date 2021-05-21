0c21fce32a80f6b18b5206155d5adf53e99110f0
hao <hao@tesosoft.com>
2021-05-21 14:12:53 +0700

    import firebase

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import MomentUtils from '@date-io/moment';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { ThemeProvider } from '@material-ui/styles';
 import 'firebase/analytics';
-import { createBrowserHistory } from 'history';
 import React from 'react';
 import { Provider as StoreProvider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
 import { BrowserRouter as Router } from 'react-router-dom';
-// const store = configureStore();
 import './App.css';
 import { CookiesNotification, ScrollReset } from './components';
-import firebase from './firebase';
+// import firebase from './firebase';
 import './mixins/moment';
 import './mixins/validate';
 import routes from './routes';
 import store from './store/configureStore';
 import theme from './theme';
 
-firebase.analytics();
-const history = createBrowserHistory();
+// firebase.analytics();
 
 const App = () => {
   return (
